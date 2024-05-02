eeff966a1d6b5084ded40756a61a72fc93f737e1
Legal <offorjohn55@gmail.com>
2024-05-02 10:16:55 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const hambuger = document.querySelector(".hambuger");
+const hambugerDrop = document.querySelector(".hambuger-drop");
+
+
+hambuger.addEventListener("click", () => {
+    hambuger.classList.toggle("active");
+    hambugerDrop.classList.toggle("active");
+  
+})
